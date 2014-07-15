4f93d467d9631f95501ad6f5c893087ab64e41ee
Christoph Schueler <cpu12.gems@googlemail.com>
2014-07-15 12:21:07 +0200

    Add 'Port_Serial.[ch]'.

---
+++ port/win32/Port_Serial.c
@@ -0,0 +1,32 @@
+/*
+ *   KONNEX/EIB-Protocol-Stack.
+ *
+ *  (C) 2007-2014 by Christoph Schueler <github.com/Christoph2,
+ *                                       cpu12.gems@googlemail.com>
+ *
+ *   All Rights Reserved
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+#include "Win_Serial.h"
+#include "Port_Serial.h"
+
+extern Win_SerialType SerialPortParams;
+
+uint8_t Port_WriteToBusInterface(uint8_t  * frame, uint16_t length)
+{
+    return Serial_Write(&SerialPortParams, frame, length);
+}
