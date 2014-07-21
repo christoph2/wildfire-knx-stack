6549d5e7abb7e1d95f0d887619cbf5497b167ccd
Christoph Schueler <cpu12.gems@googlemail.com>
2014-07-21 12:10:53 +0200

    Added 'knx_debug.[ch]'.

---
+++ inc/knx_debug.h
@@ -0,0 +1,47 @@
+
+/*
+ *   KONNEX/EIB-Protocol-Stack.
+ *
+ *  (C) 2007-2014 by Christoph Schueler <github.com/Christoph2,
+ *                                       cpu12.gems@googlemail.com>
+ *
+ *   All Rights Reserved
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+#if !defined(__KNX_DEBUG_H)
+#define __KNX_DEBUG_H
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif  /* __cplusplus */
+
+#include "k-ps/config.h"
+#include <stdint.h>
+
+#if defined(WIN32) || defined(WIN64)
+void Dbg_DumpHex(uint8_t * frame, uint16_t length);
+#else
+#define Dbg_DumpHex(frame, length)
+#endif
+
+
+#if defined(__cplusplus)
+}
+#endif  /* __cplusplus */
+
+#endif  /* __KNX_DEBUG_H */
