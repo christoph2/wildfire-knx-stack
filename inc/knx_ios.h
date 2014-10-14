da02892e6097fa6d40f78ff1e89f7b2f41d08d16
Christoph Schueler <cpu12.gems@googlemail.com>
2014-10-14 05:19:27 +0200

    [knx_ios.h] Grouped constants into enumerations.

--- inc/knx_ios.h
+++ inc/knx_ios.h
@@ -49,108 +49,114 @@ extern "C"
 /*
 **  System Interface Objects.
 */
-#define KNX_OT_DEVICE_OBJECT                ((uint16_t)0)
-#define KNX_OT_ADDRESSTABLE_OBJECT          ((uint16_t)1)
-#define KNX_OT_ASSOCIATIONTABLE_OBJECT      ((uint16_t)2)
-#define KNX_OT_APPLICATIONPROGRAM_OBJECT    ((uint16_t)3)
-#define KNX_OT_INTERFACEPROGRAM_OBJECT      ((uint16_t)4)
-#define KNX_OT_POLLING_MASTER               ((uint16_t)10)
+typedef enum tagKnx_ObjectTypeType {
+    KNX_OT_DEVICE_OBJECT                = ((uint16_t)0),
+    KNX_OT_ADDRESSTABLE_OBJECT          = ((uint16_t)1),
+    KNX_OT_ASSOCIATIONTABLE_OBJECT      = ((uint16_t)2),
+    KNX_OT_APPLICATIONPROGRAM_OBJECT    = ((uint16_t)3),
+    KNX_OT_INTERFACEPROGRAM_OBJECT      = ((uint16_t)4),
+    KNX_OT_POLLING_MASTER               = ((uint16_t)10)
+} Knx_ObjectTypeType;
 
 #define KNX_OT_USER_OBJECT0                 ((uint16_t)50001)
 
 /*
 ** Interface Object Type independent Standardized Property Identifiers.
 */
-#define KNX_PID_OBJECT_TYPE                 ((uint8_t)1)
-#define KNX_PID_OBJECT_NAME                 ((uint8_t)2)
-#define KNX_PID_SEMAPHOR                    ((uint8_t)3)
-#define KNX_PID_GROUP_OBJECT_REFERENCE      ((uint8_t)4)
-#define KNX_PID_LOAD_STATE_CONTROL          ((uint8_t)5)
-#define KNX_PID_RUN_STATE_CONTROL           ((uint8_t)6)
-#define KNX_PID_TABLE_REFERENCE             ((uint8_t)7)
-#define KNX_PID_SERVICE_CONTROL             ((uint8_t)8)
-#define KNX_PID_FIRMWARE_REVISION           ((uint8_t)9)
-#define KNX_PID_SERVICES_SUPPORTED          ((uint8_t)10)
-#define KNX_PID_SERIAL_NUMBER               ((uint8_t)11)
-#define KNX_PID_MANUFACTURER_ID             ((uint8_t)12)
-#define KNX_PID_PROGRAM_VERSION             ((uint8_t)13)
-#define KNX_PID_DEVICE_CONTROL              ((uint8_t)14)
-#define KNX_PID_ORDER_INFO                  ((uint8_t)15)
-#define KNX_PID_PEI_TYPE                    ((uint8_t)16)
-#define KNX_PID_PORT_CONFIGURATION          ((uint8_t)17)
-#define KNX_PID_POLL_GROUP_SETTINGS         ((uint8_t)18)
-#define KNX_PID_MANUFACTURER_DATA           ((uint8_t)19)
-#define KNX_PID_ENABLE                      ((uint8_t)20)
-#define KNX_PID_DESCRIPTION                 ((uint8_t)21)
-#define KNX_PID_FILE                        ((uint8_t)22)
-#define KNX_PID_GROUP_ADDRESS_LIST          ((uint8_t)23)
+typedef enum tagKnx_PropertyIdentifierType {
+    KNX_PID_OBJECT_TYPE             = ((uint8_t)1),
+    KNX_PID_OBJECT_NAME             = ((uint8_t)2),
+    KNX_PID_SEMAPHOR                = ((uint8_t)3),
+    KNX_PID_GROUP_OBJECT_REFERENCE  = ((uint8_t)4),
+    KNX_PID_LOAD_STATE_CONTROL      = ((uint8_t)5),
+    KNX_PID_RUN_STATE_CONTROL       = ((uint8_t)6),
+    KNX_PID_TABLE_REFERENCE         = ((uint8_t)7),
+    KNX_PID_SERVICE_CONTROL         = ((uint8_t)8),
+    KNX_PID_FIRMWARE_REVISION       = ((uint8_t)9),
+    KNX_PID_SERVICES_SUPPORTED      = ((uint8_t)10),
+    KNX_PID_SERIAL_NUMBER           = ((uint8_t)11),
+    KNX_PID_MANUFACTURER_ID         = ((uint8_t)12),
+    KNX_PID_PROGRAM_VERSION         = ((uint8_t)13),
+    KNX_PID_DEVICE_CONTROL          = ((uint8_t)14),
+    KNX_PID_ORDER_INFO              = ((uint8_t)15),
+    KNX_PID_PEI_TYPE                = ((uint8_t)16),
+    KNX_PID_PORT_CONFIGURATION      = ((uint8_t)17),
+    KNX_PID_POLL_GROUP_SETTINGS     = ((uint8_t)18),
+    KNX_PID_MANUFACTURER_DATA       = ((uint8_t)19),
+    KNX_PID_ENABLE                  = ((uint8_t)20),
+    KNX_PID_DESCRIPTION             = ((uint8_t)21),
+    KNX_PID_FILE                    = ((uint8_t)22),
+    KNX_PID_GROUP_ADDRESS_LIST      = ((uint8_t)23),
 
 /*
 ** Interface Object Type specific Standardized Property Identifiers.
 */
-#define KNX_PID_POLLING_STATE               ((uint8_t)51)
-#define KNX_PID_POLLING_SLAVE_ADDR          ((uint8_t)52)
-#define KNX_PID_POLL_CYCLE                  ((uint8_t)53)
+    KNX_PID_POLLING_STATE           = ((uint8_t)51),
+    KNX_PID_POLLING_SLAVE_ADDR      = ((uint8_t)52),
+    KNX_PID_POLL_CYCLE              = ((uint8_t)53),
 
 /* Extension: */
-#define KNX_PID_SYSTEM_OBJECT_EXTENSION     ((uint8_t)(0xfe))
+    KNX_PID_SYSTEM_OBJECT_EXTENSION = ((uint8_t)(0xfe))
+} Knx_PropertyIdentifierType;
 
 /*
 **  Property Data Types Identifiers.
 */
 
-#define KNX_PDT_CONTROL                     ((uint8_t)0x00)
-#define KNX_PDT_CHAR                        ((uint8_t)0x01)
-#define KNX_PDT_UNSIGNED_CHAR               ((uint8_t)0x02)
-#define KNX_PDT_INT                         ((uint8_t)0x03)
-#define KNX_PDT_UNSIGNED_INT                ((uint8_t)0x04)
-#define KNX_PDT_KNX_FLOAT                   ((uint8_t)0x05)
-#define KNX_PDT_DATE                        ((uint8_t)0x06)
-#define KNX_PDT_TIME                        ((uint8_t)0x07)
-#define KNX_PDT_LONG                        ((uint8_t)0x08)
-#define KNX_PDT_UNSIGNED_LONG               ((uint8_t)0x09)
-#define KNX_PDT_FLOAT                       ((uint8_t)0x0A)
-#define KNX_PDT_DOUBLE                      ((uint8_t)0x0B)
-#define KNX_PDT_CHAR_BLOCK                  ((uint8_t)0x0C)
-#define KNX_PDT_POLL_GROUP_SETTINGS         ((uint8_t)0x0D)
-#define KNX_PDT_SHORT_CHAR_BLOCK            ((uint8_t)0x0E)
-/* 0x0f */
-/* 0x10 */
-#define KNX_PDT_GENERIC_01                  ((uint8_t)0x11)
-#define KNX_PDT_GENERIC_02                  ((uint8_t)0x12)
-#define KNX_PDT_GENERIC_03                  ((uint8_t)0x13)
-#define KNX_PDT_GENERIC_04                  ((uint8_t)0x14)
-#define KNX_PDT_GENERIC_05                  ((uint8_t)0x15)
-#define KNX_PDT_GENERIC_06                  ((uint8_t)0x16)
-#define KNX_PDT_GENERIC_07                  ((uint8_t)0x17)
-#define KNX_PDT_GENERIC_08                  ((uint8_t)0x18)
-#define KNX_PDT_GENERIC_09                  ((uint8_t)0x19)
-#define KNX_PDT_GENERIC_10                  ((uint8_t)0x1A)
+typedef enum tagKnx_PropertyDataTypeType {
+    KNX_PDT_CONTROL             = ((uint8_t)0x00),
+    KNX_PDT_CHAR                = ((uint8_t)0x01),
+    KNX_PDT_UNSIGNED_CHAR       = ((uint8_t)0x02),
+    KNX_PDT_INT                 = ((uint8_t)0x03),
+    KNX_PDT_UNSIGNED_INT        = ((uint8_t)0x04),
+    KNX_PDT_KNX_FLOAT           = ((uint8_t)0x05),
+    KNX_PDT_DATE                = ((uint8_t)0x06),
+    KNX_PDT_TIME                = ((uint8_t)0x07),
+    KNX_PDT_LONG                = ((uint8_t)0x08),
+    KNX_PDT_UNSIGNED_LONG       = ((uint8_t)0x09),
+    KNX_PDT_FLOAT               = ((uint8_t)0x0A),
+    KNX_PDT_DOUBLE              = ((uint8_t)0x0B),
+    KNX_PDT_CHAR_BLOCK          = ((uint8_t)0x0C),
+    KNX_PDT_POLL_GROUP_SETTINGS = ((uint8_t)0x0D),
+    KNX_PDT_SHORT_CHAR_BLOCK    = ((uint8_t)0x0E),
+    /* 0x0f */
+    /* 0x10 */
+    KNX_PDT_GENERIC_01          = ((uint8_t)0x11),
+    KNX_PDT_GENERIC_02          = ((uint8_t)0x12),
+    KNX_PDT_GENERIC_03          = ((uint8_t)0x13),
+    KNX_PDT_GENERIC_04          = ((uint8_t)0x14),
+    KNX_PDT_GENERIC_05          = ((uint8_t)0x15),
+    KNX_PDT_GENERIC_06          = ((uint8_t)0x16),
+    KNX_PDT_GENERIC_07          = ((uint8_t)0x17),
+    KNX_PDT_GENERIC_08          = ((uint8_t)0x18),
+    KNX_PDT_GENERIC_09          = ((uint8_t)0x19),
+    KNX_PDT_GENERIC_10          = ((uint8_t)0x1A),
 
-/* folgendes aus 'BIM_M13x.h': */
-/*
-   #define PDT_GENERIC_11                          0x1B
-   #define PDT_GENERIC_12                          0x1C
-   #define PDT_GENERIC_13                          0x1D
-   #define PDT_GENERIC_14                          0x1E
-   #define PDT_GENERIC_15                          0x1F
-   #define PDT_GENERIC_16                          0x20
-   #define PDT_GENERIC_17                          0x21
-   #define PDT_GENERIC_18                          0x22
-   #define PDT_GENERIC_19                          0x23
-   #define PDT_GENERIC_20                          0x24
-   #define PDT_VERSION                             0x30
-   #define PDT_ALARM_INFO                          0x31
-   #define PDT_BINARY_INFORMATION                  0x32
-   #define PDT_BITSET8                             0x33
-   #define PDT_BITSET16                            0x34
-   #define PDT_ENUM8                               0x35
-   #define PDT_SCALING                             0x36
-   #define PDT_FUNCTION                            0x3E
+    /* following from 'BIM_M13x.h': */
+    KNX_PDT_GENERIC_11          = ((uint8_t)0x1B),
+    KNX_PDT_GENERIC_12          = ((uint8_t)0x1C),
+    KNX_PDT_GENERIC_13          = ((uint8_t)0x1D),
+    KNX_PDT_GENERIC_14          = ((uint8_t)0x1E),
+    KNX_PDT_GENERIC_15          = ((uint8_t)0x1F),
+    KNX_PDT_GENERIC_16          = ((uint8_t)0x20),
+    KNX_PDT_GENERIC_17          = ((uint8_t)0x21),
+    KNX_PDT_GENERIC_18          = ((uint8_t)0x22),
+    KNX_PDT_GENERIC_19          = ((uint8_t)0x23),
+    KNX_PDT_GENERIC_20          = ((uint8_t)0x24),
+    KNX_PDT_VERSION             = ((uint8_t)0x30),
+    KNX_PDT_ALARM_INFO          = ((uint8_t)0x31),
+    KNX_PDT_BINARY_INFORMATION  = ((uint8_t)0x32),
+    KNX_PDT_BITSET8             = ((uint8_t)0x33),
+    KNX_PDT_BITSET16            = ((uint8_t)0x34),
+    KNX_PDT_ENUM8               = ((uint8_t)0x35),
+    KNX_PDT_SCALING             = ((uint8_t)0x36),
+    KNX_PDT_FUNCTION            = ((uint8_t)0x3E)
+} Knx_PropertyDataTypeType;
 
-   #define PDT_WR_EN                               0x80
-   #define FUNCTION_FLAG                           0x40
- */
+
+   #define PDT_WR_EN            0x80
+   #define FUNCTION_FLAG        0x40
+// */
 
 /*
 ** Global function-like macros.
