533eef954cee9edfd96d0da135280757d54de839
Christoph Schueler <cpu12.gems@googlemail.com>
2016-06-06 07:21:48 +0100

    Add mock functions.

---
+++ src/port/mock/mock_sync.c
@@ -0,0 +1,63 @@
+/*
+*   Wildfire - The Open Source KNX/EIB-Protocol Stack.
+*
+*  (C) 2007-2016 by Christoph Schueler <github.com/Christoph2,
+*                                       cpu12.gems@googlemail.com>
+*
+*   All Rights Reserved
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free Software Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*/
+
+
+
+typedef struct tagSync_tagMutexType {
+  int dummy;
+} Sync_MutexType;
+
+
+void Sync_Init(Sync_MutexType * mutex)
+{
+}
+
+void Sync_Deinit(Sync_MutexType * mutex)
+{
+}
+
+void Sync_Lock(Sync_MutexType * mutex)
+{
+}
+
+void Sync_Unlock(Sync_MutexType * mutex)
+{
+}
+
+void Sync_TryLock(Sync_MutexType * mutex)
+{
+}
+
+void Sync_Setup(void)
+{
+}
+
+void Sync_Lock_TaskLevel(void)
+{
+}
+
+void Sync_Unlock_TaskLevel(void)
+{
+}
+
---
+++ src/port/mock/mock_timer.c
@@ -0,0 +1,46 @@
+/*
+*   Wildfire - The Open Source KNX/EIB-Protocol Stack.
+*
+*  (C) 2007-2016 by Christoph Schueler <github.com/Christoph2,
+*                                       cpu12.gems@googlemail.com>
+*
+*   All Rights Reserved
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free Software Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*/
+
+
+void Port_Timer_Init(Port_Timer_ConfigType const * const config)
+{
+}
+
+
+void Port_Timer_Deinit(void)
+{
+}
+
+void Port_Timer_Start(long millis)
+{
+}
+
+void Port_Timer_Stop(void)
+{
+}
+
+void Port_Timer_Setup(void)
+{
+}
+
