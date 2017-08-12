89a86bab2e15011af8e7fa5b8a42bced8e65c18a
Christoph Schueler <cpu12.gems@googlemail.com>
2017-08-12 16:59:40 +0100

    Add 'knx_types.hpp'.

---
+++ inc/knx_defs.hpp
@@ -0,0 +1,152 @@
+/*
+*   Wildfire - The Open Source KNX/EIB-Protocol Stack.
+*
+*  (C) 2007-2017 by Christoph Schueler <github.com/Christoph2,
+*                                       cpu12.gems@googlemail.com>
+*
+*   All Rights Reserved
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more Details.
+*
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free Software Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*/
+#if !defined(__KNX_DEFS_HPP)
+#define __KNX_DEFS_HPP
+
+#include "knx_types.h"
+#include "knx_macros.h"
+#include "knx_utl.h"
+#include "knx_tracelog.h"
+#include "knx_moduleids.h"
+
+namespace knx {
+
+/*
+** Global defines.
+*/
+#define IAK_OK                  ((uint8_t)0x00)
+#define IAK_NOT_OK              ((uint8_t)0x01)
+
+/*
+   #define MSG_NO_ROUTING_CTRL ((BYTE)7)
+   NoRouting,Parameter,Force,
+ */
+
+/* RunError-Codes. */
+#define KNX_RUNERROR_SYS3_ERR   ((uint8_t)0x40) /* internal system error (not for each default MCB [ROM] an. */
+                                                /* MCB [EEPROM] exists with the same start address). */
+#define KNX_RUNERROR_SYS2_ERR   ((uint8_t)0x20) /* internal system error (stuck/ overheat detection). */
+#define KNX_RUNERROR_OBJ_ERR    ((uint8_t)0x10) /* RAM flag table error (*PTR not in ZPAGE range). */
+#define KNX_RUNERROR_STK_OVL    ((uint8_t)0x08) /* stack overflow was detected. */
+#define KNX_RUNERROR_EEPROM_ERR ((uint8_t)0x04) /* EEPROM check detected an CRC error. */
+#define KNX_RUNERROR_SYS1_ERR   ((uint8_t)0x02) /* internal system error (parity bit in SystemState is corrupt). */
+#define KNX_RUNERROR_SYS0_ERR   ((uint8_t)0x01) /* internal system error (msg buffer offset corrupt). */
+
+/*
+** Config-Byte / Object-Descriptor.
+*/
+#define KNX_OBJ_UPDATE_ENABLE   ((uint8_t)0x80) /* GroupValueRead()_Res wird verarbeitet - nur Mask 0020h, 0021h. */
+#define KNX_OBJ_TRANSMIT_ENABLE ((uint8_t)0x40) /* Lese- und Schreib-Anforderungen (Requests) vom App.-Layer werden verarbeitet. */
+#define KNX_OBJ_EEPROM_VALUE    ((uint8_t)0x20) /* Objektwert steht im EEPROM (statt im RAM). */
+#define KNX_OBJ_WRITE_ENABLE    ((uint8_t)0x10) /* Objektwert kann über den Bus geschrieben werden. */
+#define KNX_OBJ_READ_ENABLE     ((uint8_t)0x08) /* Objektwert kann über den Bus gelesen werden. */
+#define KNX_OBJ_COMM_ENABLE     ((uint8_t)0x04) /* "Mainswitch" for Communikation. */
+
+/*
+**  Config-Byte / Priorities.
+*/
+#define KNX_OBJ_PRIO_SYSTEM     ((uint8_t)0)
+#define KNX_OBJ_PRIO_URGENT     ((uint8_t)2)
+#define KNX_OBJ_PRIO_NORMAL     ((uint8_t)1)
+#define KNX_OBJ_PRIO_LOW        ((uint8_t)3)
+
+/*
+    Transmission Priorities
+    =======================
+    11 = low operational priority
+    10 = high operational priority
+    01 = alarm priority
+    00 = system priority
+ */
+
+/*
+** Comm-Flags.
+*/
+
+#define KNX_OBJ_UPDATED                 ((uint8_t)0x08)
+#define KNX_OBJ_DATA_REQUEST            ((uint8_t)0x04)
+
+/*
+** Transmission-Status
+*/
+#define KNX_OBJ_IDLE_OK                 ((uint8_t)0x00)
+#define KNX_OBJ_IDLE_ERROR              ((uint8_t)0x01)
+#define KNX_OBJ_TRANSMITTING            ((uint8_t)0x02)
+#define KNX_OBJ_TRANSMIT_REQ            ((uint8_t)0x03)
+
+#define BCU20_PRIVILEGE_CONFIGURATION   ((uint8_t)0)
+#define BCU20_PRIVILEGE_SERVICE         ((uint8_t)1)
+#define BCU20_PRIVILEGE_USER            ((uint8_t)2)
+#define BCU20_PRIVILEGE_NO              ((uint8_t)3)
+
+#define KNX_UNUSED_TSAP                 ((uint8_t)0xfe)
+#define KNX_INVALID_TSAP                ((uint8_t)0x00)
+
+
+/**
+ * Supported Bus Interfaces.
+ */
+#define KNX_BIF_TPUART_1                        (0)
+#define KNX_BIF_TPUART_2                        (1)
+#define KNX_BIF_NCN5120                         (2)
+
+
+/** Power supply of controller module.
+ *
+ */
+#define KNX_MODULE_POWER_BY_BUS                 (0)
+#define KNX_MODULE_POWER_BY_AUXILIARY_SUPPLY    (1)
+
+/** Endianess
+ *
+ */
+#define KNX_LITTLE_ENDIAN                       (0)
+#define KNX_BIG_ENDIAN                          (1)
+
+
+/** Build types.
+ *
+ */
+#define KNX_BUILD_DEBUG                         (0)
+#define KNX_BUILD_RELEASE                       (1)
+
+/** Target Types.
+ *
+ */
+#define KNX_TARGET_WINDOWS                      (1)
+#define KNX_TARGET_POSIX                        (2)
+#define KNX_TARGET_S12                          (3)
+#define KNX_TARGET_ATMEL_ATSAM4                 (4)
+#define KNX_TARGET_ATMEL_ATMEGA                 (5)
+
+/** Stack Types.
+ *
+ */
+#define KNX_LINK_LAYER_ONLY			(1)
+#define KNX_FULL_STACK				(2)
+
+} // namespace knx
+
+#endif  /* __KNX_DEFS_HPP */
+
---
+++ inc/knx_types.hpp
@@ -0,0 +1,129 @@
+/*
+*   Wildfire - The Open Source KNX/EIB-Protocol Stack.
+*
+*  (C) 2007-2017 by Christoph Schueler <github.com/Christoph2,
+*                                       cpu12.gems@googlemail.com>
+*
+*   All Rights Reserved
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free Software Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*/
+#if !defined(__KNX_TYPES_HPP)
+#define __KNX_TYPES_HPP
+
+#include <cstdint>
+#include "knx_imi.hpp"
+
+namespace knx {
+
+/*
+ *
+ * Basic Types.
+ *
+ */
+using float32 = float;
+using float64 = double;
+
+#define STD_ON      (1)
+#define STD_OFF     (0)
+
+/*
+** Global types.
+*/
+using Knx_AddressType = uint16_t;
+using Knx_SerialNumberType = uint8_t[6];
+
+enum class Knx_StatusType {
+    KNX_E_OK,
+    KNX_E_NOT_OK
+};
+
+struct Knx_PollGroupSettingType {
+    uint16_t group_addr;
+    uint8_t slot_no;
+};
+
+struct Knx_CommObjDescriptorType {
+    uint8_t dataPtr;
+    uint8_t config;
+    uint8_t type;
+};
+
+enum class Knx_DafType : uint8_t {
+    KNX_ADDR_INDIVIDUAL = 0x00,
+    KNX_ADDR_MULTICAST  = 0x80
+};
+
+enum class Knx_FrameTypeType : uint8_t {
+    KNX_FRAME_EXTENDED  = 0x00,
+    KNX_FRAME_STANDARD  = 0x80,
+    KNX_FRAME_POLLING   = 0xC0
+};
+
+enum class Knx_ObjectType : uint8_t {
+    KNX_OT_UINT1,
+    KNX_OT_UINT2,
+    KNX_OT_UINT3,
+    KNX_OT_UINT4,
+    KNX_OT_UINT5,
+    KNX_OT_UINT6,
+    KNX_OT_UINT7,
+    KNX_OT_UINT8,
+    KNX_OT_UINT16,
+    KNX_OT_BYTE3,
+    KNX_OT_FLOAT,
+    KNX_OT_DATA6,
+    KNX_OT_DOUBLE,
+    KNX_OT_DATA10,
+    KNX_OT_MAXDATA,
+    KNX_OT_VARDATA
+};
+
+using Knx_PriorityType = uint8_t;
+
+/*
+**  START: LAYER-Dispatching-Functions.
+*/
+using Knx_LayerServiceFunctionType  = void (*)();
+
+struct Knx_ServiceEntryType {
+    Knx_ServiceTypeType Service;
+    Knx_LayerServiceFunctionType Function;
+};
+
+struct Knx_LayerServicesType {
+    uint8_t LayerOffset;
+    uint8_t NumServices;
+    const Knx_LayerServiceFunctionType * Functions;
+};
+/*
+**  END: LAYER-Dispatching-Functions.
+*/
+
+using Knx_HopCountType = uint8_t;
+
+/*
+**  Global Types.
+*/
+enum class KnxModule_StateType {
+    KNX_MODULE_UNINIT,
+    KNX_MODULE_READY
+};
+
+} // namespace knx
+
+#endif  /* __KNX_TYPES_HPP */
+
