8253a7338200c9f42ee724fe9f7089797ce069f2
Christoph Schueler <cpu12.gems@googlemail.com>
2017-08-14 17:49:11 +0100

    Add 'knx_layer_link.hpp'.

---
+++ inc/knx_layer_link.hpp
@@ -0,0 +1,37 @@
+/*
+*   Wildfire - The Open Source KNX/EIB-Protocol Stack.
+*
+*  (C) 2007-2017 by Christoph Schueler <github.com/Christoph2,
+*                                       cpu12.gems@googlemail.com>
+*
+*   All Rights Reserved
+*
+*  This program is free softwKNXe; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free SoftwKNXe Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WKNXRANTY; without even the implied wKNXranty of
+*  MERCHANTABILITY or FITNESS FOR A PKNXTICULKNX PURPOSE.  See the
+*  GNU General Public License for more KnxEtails.
+*
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free SoftwKNXe Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*/
+#if !defined(__KNX_LAYER_LINK_HPP)
+#define __KNX_LAYER_LINK_HPP
+
+#include "knx_messaging.hpp"
+#include "knx_disp.hpp"
+
+#include "link-layer/uart_bif.hpp"    /* Only UART bus interfaces for now. */
+
+namespace knx {
+
+} // namespace knx
+
+#endif  /* __KNX_LAYER_LINK_HPP */
+
