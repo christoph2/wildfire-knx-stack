5dc55592fa4274d9643c61316de8bd760ba7ea05
Christoph Schueler <cpu12.gems@googlemail.com>
2017-08-14 16:37:38 +0100

    Add 'knx_disp.hpp'.

---
+++ inc/knx_disp.hpp
@@ -0,0 +1,52 @@
+/*
+*   Wildfire - The Open Source KNX/EIB-Protocol Stack.
+*
+*  (C) 2007-2017 by Christoph Schueler <github.com/Christoph2,
+*                                       cpu12.gems@googlemail.com>
+*
+*   All Rights Reserved
+*
+*  This program is free softwKNXe; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free SoftwKNXe Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WKNXRANTY; without even the implied wKNXranty of
+*  MERCHANTABILITY or FITNESS FOR A PKNXTICULKNX PURPOSE.  See the
+*  GNU General Public License for more KnxEtails.
+*
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free SoftwKNXe Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*/
+#if !defined(__KNX_DISP_HPP)
+#define __KNX_DISP_HPP
+
+#include "knx_messaging.hpp"
+
+namespace knx {
+
+/*
+** Global variables.
+*/
+extern KnxMsg_Buffer * KnxMsg_ScratchBufferPtr;
+
+/*
+** Global functions.
+*/
+#if KSTACK_MEMORY_MAPPING == STD_ON
+FUNC(void, KSTACK_CODE) KnxDisp_DispatchLayer(const uint8_t LayerID,
+                                              CONSTP2CONST(Knx_LayerServicesType, AUTOMATIC, KSTACK_APPL_DATA) ServiceTable
+                                              );
+#else
+void KnxDisp_DispatchLayer(const uint8_t LayerID, const Knx_LayerServicesType * ServiceTable);
+
+
+#endif /* KSTACK_MEMORY_MAPPING */
+
+} // namespace knx
+
+#endif  /* __KNX_DISP_HPP */
+
