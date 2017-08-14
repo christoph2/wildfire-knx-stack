ab31ba09e8eac3fc86c943b3f4e004647090868e
Christoph Schueler <cpu12.gems@googlemail.com>
2017-08-14 17:09:05 +0100

    Add 'knx_et.hpp'.

---
+++ inc/knx_et.hpp
@@ -0,0 +1,54 @@
+/*
+*   Wildfire - The Open Source KNX/EIB-Protocol Stack.
+*
+*  (C) 2007-2017 by Christoph Schueler <github.com/Christoph2,
+*                                       cpu12.gems@googlemail.com>
+*
+*   All Rights Reserved
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more KnxEtails.
+*
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free Software Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*/
+
+#if !defined(__KNX_ET_HPP)
+#define __KNX_ET_HPP
+
+#include "knx_defs.hpp"
+
+namespace knx {
+
+using KnxEt_CallbackType = void (*)(uint8_t ModuleId, uint8_t ApiId, uint8_t ErrorCode);
+
+struct KnxEt_ErrorConditionType {
+    uint8_t ModuleId;
+    uint8_t ApiId;
+    uint8_t ErrorCode;
+};
+
+
+void KnxEt_Init(void);
+void KnxEt_ReportError(uint8_t ModuleId, uint8_t ApiId, uint8_t ErrorCode);
+void KnxEt_Start(void);
+
+void KnxEt_SetCallback(KnxEt_CallbackType const * const callback);
+void KnxEt_GetErrorCondition(KnxEt_ErrorConditionType * condition);
+
+void KnxEt_Error(char * function, uint32_t err);
+void KnxEt_DumpHex(uint8_t const * frame, uint16_t length);
+
+} // namespace knx
+
+#endif  /* __KNX_ET_HPP */
+
