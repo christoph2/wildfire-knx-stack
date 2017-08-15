b8e22b654b4322497558b25b32fba1115667e1e7
Christoph Schueler <cpu12.gems@googlemail.com>
2017-08-15 14:28:59 +0100

    Add 'knx_sched.hpp'.

---
+++ inc/knx_sched.hpp
@@ -0,0 +1,59 @@
+/*
+*   Wildfire - The Open Source KNX/EIB-Protocol Stack.
+*
+*  (C) 2007-2017 by Christoph Schueler <github.com/Christoph2,
+*                                       cpu12.gems@googlemail.com>
+*
+*   All Rights Reserved
+*
+*  This program is free softwKNXe; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free SoftwKNXe Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WKNXRANTY; without even the implied wKNXranty of
+*  MERCHANTABILITY or FITNESS FOR A PKNXTICULKNX PURPOSE.  See the
+*  GNU General Public License for more KnxEtails.
+*
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, write to the Free SoftwKNXe Foundation, Inc.,
+*  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*/
+#if !defined(__KNX_SCHED_HPP)
+#define __KNX_SCHED_HPP
+
+#include "knx_messaging.hpp"
+#include "knx_disp.hpp"
+
+namespace knx {
+
+
+/*
+** Global defines.
+*/
+#define KNX_DEFAULT_HOP_COUNT ((uint8_t)6)
+
+
+/*
+** Global functions.
+*/
+#if KSTACK_MEMORY_MAPPING == STD_ON
+FUNC(void, KSTACK_CODE)	KnxSched_Task(void);
+FUNC(void, KSTACK_CODE)	KnxSched_Init(void);
+#else	
+void KnxSched_Task(void);
+void KnxSched_Init(void);
+
+void KnxUser_Main(void);    /* CHECK: Separate module? */
+void KnxUser_Init(void);
+void KnxUser_Save(void);
+
+#endif /* KSTACK_MEMORY_MAPPING */
+
+
+} // namespace knx
+
+#endif  /*__KNX_SCHED_HPP*/
+
